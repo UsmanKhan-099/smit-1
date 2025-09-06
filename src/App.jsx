3186d2f7ec61def0ba5d023c13e22355fbfa6072
UsmanKhan-099git config --global user.email techwisecourses732161@gmail.comgit config user.namegit config --global user.name UsmanKhan-099 <techwisecourses732161@gmail.com>
2025-09-06 23:24:10 +0500

    changes done in routing

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <>
      <Routes>
       <Route path="/" element={<Login />}/>
+      <Route path="/login" element={<Login />}/>
       <Route path="/register" element={<Register />}/>
       <Route path="/products" element={<Products />}/>
      </Routes>
